c2001d43471134dc25d1d03c4fdf4d1779ce97f8
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2020-08-18 17:06:22 -0300

    add white list params

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -4,7 +4,7 @@ Knock.setup do |config|
 
   config.token_signature_algorithm = 'HS256'
 
-  config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
+  config.token_secret_signature_key = -> { ENV['SECRET_KEY_BASE'] }
 
   config.not_found_exception_class_name = 'ActiveRecord::RecordNotFound'
 end
